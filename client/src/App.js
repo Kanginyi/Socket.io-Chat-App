db1a83135fa75925b3c830940adbbb688fde7438
Eric Yi <erickanginyi@gmail.com>
2022-02-03 15:03:37 -0500

    Create initial connection to socket backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
 
+// io is going to be used to establish a connection to Socket.io
+import io from "socket.io-client";
+
+// Similar to how it is in the backend, we want to create a connection from the frontend to the backend server
+const socket = io.connect("http://localhost:3001");
+
 function App() {
   return (
     <div className="App">
