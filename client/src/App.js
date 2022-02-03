5c6f13615f911b1b2a708dc7e13f678738d2a73c
Eric Yi <erickanginyi@gmail.com>
2022-02-03 18:38:25 -0500

    Conditionally render either chatroom/login

--- client/src/App.js
+++ client/src/App.js
@@ -11,33 +11,39 @@ const socket = io.connect("http://localhost:3001");
 function App() {
    const [username, setUsername] = useState("");
    const [room, setRoom] = useState("");
+   const [showChatroom, setShowChatroom] = useState(false);
 
    // onClick to join the room that we want to 
    const joinRoom = () => {
       if (username !== "" && room !== "") {
          socket.emit("join_room", room)// First event call from the front end. Using .emit(), we can access the thing in the backend with the same name ("join_room") and then pass in the matching data in the backend by passing in a second argument, in this case, "room"
+         setShowChatroom(prev => !prev);
       }
    }
 
    return (
       <div className="App">
-         <h3>Join A Chatroom</h3>
+         {showChatroom ?
+            <Chat socket={socket} username={username} room={room}/>
+         : 
+            <div className="join-chat-container">
+               <h3>Join A Chatroom</h3>
 
-         <input
-            onChange={e => setUsername(e.target.value)}
-            type="text"
-            placeholder="Enter a username"
-         />
+               <input
+                  onChange={e => setUsername(e.target.value)}
+                  type="text"
+                  placeholder="Enter a username"
+               />
 
-         <input
-            onChange={e => setRoom(e.target.value)}
-            type="text"
-            placeholder="Enter Room ID"
-         />
+               <input
+                  onChange={e => setRoom(e.target.value)}
+                  type="text"
+                  placeholder="Enter Room ID"
+               />
 
-         <button onClick={joinRoom}>Join Room</button>
-
-         <Chat socket={socket} username={username} room={room}/>
+               <button onClick={joinRoom}>Join Room</button>
+            </div>
+         }
       </div>
    );
 }
