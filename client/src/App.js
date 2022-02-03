10bbf5da0d33c59223e3986602a38151b131af27
Eric Yi <erickanginyi@gmail.com>
2022-02-03 15:19:30 -0500

    Connect frontend to backend using socket.emit()

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,41 @@ import './App.css';
 
 // io is going to be used to establish a connection to Socket.io
 import io from "socket.io-client";
+import { useState } from "react";
 
 // Similar to how it is in the backend, we want to create a connection from the frontend to the backend server
 const socket = io.connect("http://localhost:3001");
 
 function App() {
-  return (
-    <div className="App">
-       asdf
-    </div>
-  );
+   const [username, setUsername] = useState("");
+   const [room, setRoom] = useState("");
+
+   // onClick to join the room that we want to 
+   const joinRoom = () => {
+      if (username !== "" && room !== "") {
+         socket.emit("join_room", room)// First event call from the front end. Using .emit(), we can access the thing in the backend with the same name ("join_room") and then pass in the matching data in the backend by passing in a second argument, in this case, "room"
+      }
+   }
+
+   return (
+      <div className="App">
+         <h3>Join A Chatroom</h3>
+
+         <input
+            onChange={e => setUsername(e.target.value)}
+            type="text"
+            placeholder="Enter a username"
+         />
+
+         <input
+            onChange={e => setRoom(e.target.value)}
+            type="text"
+            placeholder="Enter Room ID"
+         />
+
+         <button onClick={joinRoom}>Join Room</button>
+      </div>
+   );
 }
 
 export default App;
