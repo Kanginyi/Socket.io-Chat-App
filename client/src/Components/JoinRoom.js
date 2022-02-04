3982b9b37d2e02ae3a8b7c0b0382e397b292cc0a
Eric Yi <erickanginyi@gmail.com>
2022-02-04 18:49:23 -0500

    Create file, import code from App

---
+++ client/src/Components/JoinRoom.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function JoinRoom({joinRoom, setUsername, setRoom}) {
+   return (
+      <div className="join-chat-container">
+         <h3>Join A Chatroom</h3>
+
+         <input
+            onChange={e => setUsername(e.target.value)}
+            type="text"
+            placeholder="Enter a username"
+         />
+
+         <input
+            onChange={e => setRoom(e.target.value)}
+            type="text"
+            placeholder="Enter Room ID"
+         />
+
+         <button onClick={joinRoom}>Join Room</button>
+      </div>
+   );
+}
+
+export default JoinRoom;
