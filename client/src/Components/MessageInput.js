ca255a958ef21dbcd0e7f1f36eee8c2e0fcf7b26
Eric Yi <erickanginyi@gmail.com>
2022-02-04 19:39:20 -0500

    Create file, import code from Chat.js

---
+++ client/src/Components/MessageInput.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function MessageInput({setCurrentMessage, currentMessage, sendMessage}) {
+   return (
+      <div className="chat-footer">
+         <input
+            onChange={e => setCurrentMessage(e.target.value)}
+            type="text"
+            placeholder="Enter a message"
+            value={currentMessage}
+            onKeyPress={e => {e.key === "Enter" && sendMessage()}}
+         />
+
+         <button onClick={sendMessage}>&#9658;</button>
+      </div>
+   );
+}
+
+export default MessageInput;
