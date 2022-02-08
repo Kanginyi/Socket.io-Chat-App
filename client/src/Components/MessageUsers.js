24ebbb8f3cd554a50730d7bdfdeb0e66c357e60a
Eric Yi <erickanginyi@gmail.com>
2022-02-08 15:45:06 -0500

    Create file, populate with placeholder structure

---
+++ client/src/Components/MessageUsers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MessageUsers() {
+   return (
+      <details className="">
+         <summary>Bingo</summary>
+      </details>
+   );
+}
+
+export default MessageUsers;
