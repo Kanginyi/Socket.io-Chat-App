b001960b90df84e7055ffd06dd82c43775543978
Eric Yi <erickanginyi@gmail.com>
2022-02-09 06:04:40 -0500

    Pass down props, create onClick function, create and render test elements
    Take in socket, username, room, allUsers, setShowChatroom as props
    Create leaveRoom function to emit to the 'leave_room' listener with object containing {username, room}
    Use setShowChatroom to render the JoinRoom.js component
    Map over allUsers to test stylings
    Create button to test leaveRoom function

--- client/src/Components/MessageUsers.js
+++ client/src/Components/MessageUsers.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-function MessageUsers() {
+function MessageUsers({socket, username, room, allUsers, setShowChatroom}) {
+   const leaveRoom = () => {
+      socket.emit("leave_room", {username, room})
+
+      setShowChatroom(false);
+   }
+
    return (
       <details className="">
          <summary>Bingo</summary>
+         {allUsers.map(data => <p>{data.username}</p>)}
+         <button onClick={leaveRoom}>Leave Room</button>
       </details>
    );
 }
