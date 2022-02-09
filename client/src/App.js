695bfca5d7aebf2c61b7f8e2d9d7d50d185e12fe
Eric Yi <erickanginyi@gmail.com>
2022-02-09 05:54:51 -0500

    Add allUsers, create socket.on listener, pass down props
    Create allUsers using useState
    Use useEffect to take in the socket.on(all_users) and update the state of allUsers
    Pass down the props of allUsers, setShowChatRoom

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import JoinRoom from "./Components/JoinRoom";
 
 // io is going to be used to establish a connection to Socket.io
 import io from "socket.io-client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // Similar to how it is in the backend, we want to create a connection from the frontend to the backend server
 const socket = io.connect("http://localhost:3001");
@@ -13,16 +13,23 @@ function App() {
    const [username, setUsername] = useState("");
    const [room, setRoom] = useState("");
    const [showChatroom, setShowChatroom] = useState(false);
+   const [allUsers, setAllUsers] = useState([]);
 
    const currentUser = socket.id;
-   
+  
    // onClick to join the room that we want to 
    const joinRoom = () => {
       if (username !== "" && room !== "") {
-         socket.emit("join_room", room)// First event call from the front end. Using .emit(), we can access the thing in the backend with the same name ("join_room") and then pass in the matching data in the backend by passing in a second argument, in this case, "room"
+         socket.emit("join_room", {username, room, currentUser})// First event call from the front end. Using .emit(), we can access the thing in the backend with the same name ("join_room") and then pass in the matching data in the backend by passing in a second argument, in this case, "room"
          setShowChatroom(prev => !prev);
       }
    }
+
+   useEffect(() => {
+      socket.on("all_users", data => {
+         setAllUsers(data);
+      })
+   }, [socket]);
 
    const homeHeader = <h1 id="spacenook-font-size" className="spacenook-header">
                         Welcome to SpaceNook Messenger
@@ -43,6 +50,8 @@ function App() {
                   username={username}
                   room={room}
                   currentUser={currentUser}
+                  allUsers={allUsers}
+                  setShowChatroom={setShowChatroom}
                />
             : 
                <JoinRoom
