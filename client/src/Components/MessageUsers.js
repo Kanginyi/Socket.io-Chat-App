d3f8953c7b9eb74d7ffa95666e998a0cd06aee48
Eric Yi <erickanginyi@gmail.com>
2022-02-10 16:47:19 -0500

    Create showUsers function, add button, import react-icons, conditionally render icons/divs
    Oops, forgot to mention we imported useState
    Use said useState to create showAllUsers with its inherent value set to the boolean value of false
    
    Create showUsers function to toggle showAllUsers from false to true and vice versa
    
    Create parent div to hold elements
    Create button and add onClick to showUsers function
    
    Populate button with conditionally rendered imported ArrowUp and ArrowDown react-icons
    
    Conditionally render different divs based on showAllUsers boolean value

--- client/src/Components/MessageUsers.js
+++ client/src/Components/MessageUsers.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import {BsArrowUpCircleFill, BsArrowDownCircleFill} from "react-icons/bs";
 
 function MessageUsers({allUsers}) {
+   const [showAllUsers, setShowAllUsers] = useState(false);
+
    const renderUsers = allUsers?.map(user => {
       return <p>{user.username}</p>
    });
 
+   const showUsers = () => {
+      setShowAllUsers(prev => !prev);
+   }
+
    return (
-
       <div className="chat-users">
-         <div>
-            {renderUsers}
-         </div>
-      </div>
+         <button onClick={showUsers}>
+            {
+               showAllUsers
+               ?
+               <BsArrowUpCircleFill className="show-users-circle" title="Hide All Users"/>
+               :
+               <BsArrowDownCircleFill className="show-users-circle" title="Show All Users"/>
+            }
+         </button>
 
+         {
+               showAllUsers
+            ?
+               <div className="show-all-users">
+                  {renderUsers}
+               </div>
+            : 
+               <div className="chat-dropdown">
+                  Show All Users
+               </div>
+         }
+      </div>
    );
 }
 
