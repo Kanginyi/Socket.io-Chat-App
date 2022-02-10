27995791870424a3eb63f048b129f192acbd4e56
Eric Yi <erickanginyi@gmail.com>
2022-02-09 20:59:28 -0500

    Take in props, import leaveRoom function, create button
    Take in socket, username, room, setShowChatroom
    Import leaveRoom function from MessageUsers component
    Create and render button onto Dom

--- client/src/Components/MessagesList.js
+++ client/src/Components/MessagesList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Message from "./Message";
 import ScrollToBottom from "react-scroll-to-bottom";
 
-function MessagesList({allMessages, currentUser}) {
+function MessagesList({allMessages, currentUser, socket, username, room, setShowChatroom}) {
    const renderMessage = allMessages?.map(messageSent => {
       return <Message
                messageSent={messageSent}
@@ -10,8 +10,16 @@ function MessagesList({allMessages, currentUser}) {
              />
    });
 
+   const leaveRoom = () => {
+      socket.emit("leave_room", {username, room})
+
+      setShowChatroom(false);
+   }
+
    return (
       <div className="chat-body">
+         <button onClick={leaveRoom} className="leave-room">Leave Room</button>
+
          <ScrollToBottom className="messages-list">
             {renderMessage}
          </ScrollToBottom>
